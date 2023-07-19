63432ec4917f9e3baf79d0b75ddf25a6925b61e1
Chaitanya-Chaurasia <98615500+Chaitanya-Chaurasia@users.noreply.github.com>
2023-07-19 19:34:05 +0530

    Added news-item card. Rendered newsAPI response in each card

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,17 +2,29 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description } = this.props;
+    let { title, description, imageURL, url } = this.props;
 
     return (
-      <div>
+      <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src="#" className="card-img-top" alt="..." />
+          <img
+            src={
+              imageURL
+                ? imageURL
+                : "https://media.istockphoto.com/id/1264074047/vector/breaking-news-background.jpg?s=612x612&w=0&k=20&c=C5BryvaM-X1IiQtdyswR3HskyIZCqvNRojrCRLoTN0Q="
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
+            <p className="card-text">
+              {!description
+                ? "Click `Read More` to read"
+                : description.slice(0, 88)}
+            </p>
+            <a href={url} target="_blank" className="btn btn-sm btn-dark">
+              Read More
             </a>
           </div>
         </div>
