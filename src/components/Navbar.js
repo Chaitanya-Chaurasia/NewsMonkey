b06da104947f70c56ebd9c30a07fba96046c41f5
Chaitanya-Chaurasia <98615500+Chaitanya-Chaurasia@users.noreply.github.com>
2023-07-22 10:31:44 +0530

    Fixed Navbar, added unique keys to force re-mount element on switching pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,79 +1,77 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export class Navbar extends Component {
   render() {
     return (
       <div>
-        <Router>
-          <nav
-            className="navbar navbar-expand-lg"
-            style={{
-              background: "#c9ffbf",
-              background: "-webkit-linear-gradient(to right, #c9ffbf, #ffafbd)",
-              background: "linear-gradient(to right, #c9ffbf, #ffafbd)",
-            }}
-          >
-            <div className="container-fluid">
-              <Link className="navbar-brand" to="#">
-                <strong>News</strong>Monkey
-              </Link>
-              <button
-                className="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <div
-                className="collapse navbar-collapse"
-                id="navbarSupportedContent"
-              >
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/">
-                      Top
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="Business">
-                      Business
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="Entertainment">
-                      Entertainment
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="Health">
-                      Health
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="Science">
-                      Science
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="Sports">
-                      Sports
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="Tech">
-                      Tech
-                    </Link>
-                  </li>
-                </ul>
-              </div>
+        <nav
+          className="navbar navbar-expand-lg"
+          style={{
+            background: "#c9ffbf",
+            background: "-webkit-linear-gradient(to right, #c9ffbf, #ffafbd)",
+            background: "linear-gradient(to right, #c9ffbf, #ffafbd)",
+          }}
+        >
+          <div className="container-fluid">
+            <Link className="navbar-brand" to="/">
+              <strong>News</strong>Monkey
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="business">
+                    Business
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="entertainment">
+                    Entertainment
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="health">
+                    Health
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="science">
+                    Science
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="sports">
+                    Sports
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="technology">
+                    Tech
+                  </Link>
+                </li>
+              </ul>
             </div>
-          </nav>
-        </Router>
+          </div>
+        </nav>
       </div>
     );
   }
