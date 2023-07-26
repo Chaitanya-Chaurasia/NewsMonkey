231506d87a1ee0b1d59864c3c3cfeead20e25a73
Chaitanya-Chaurasia <98615500+Chaitanya-Chaurasia@users.noreply.github.com>
2023-07-26 11:14:22 +0530

    Added top loading bar and fixed Spinner issue

--- src/App.js
+++ src/App.js
@@ -2,21 +2,34 @@ import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import News from "./components/News";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
 
 export class App extends Component {
+  pageSize = 4;
+
+  state = {
+    progress: 0,
+  };
+
+  setProgress = (progress) => {
+    this.setState({ progress: progress });
+  };
+
   render() {
     return (
       <div>
         <Router>
           <Navbar />
+          <LoadingBar color="#f11946" progress={this.state.progress} />
           <Routes>
             <Route
               exact
               path="/"
               element={
                 <News
+                  setProgress={this.setProgress}
                   key="general"
-                  pageSize={4}
+                  pageSize={this.pageSize}
                   country="in"
                   category="general"
                 />
@@ -27,8 +40,9 @@ export class App extends Component {
               path="/science"
               element={
                 <News
+                  setProgress={this.setProgress}
                   key="science"
-                  pageSize={4}
+                  pageSize={this.pageSize}
                   country="in"
                   category="science"
                 />
@@ -39,8 +53,9 @@ export class App extends Component {
               path="/health"
               element={
                 <News
+                  setProgress={this.setProgress}
                   key="health"
-                  pageSize={4}
+                  pageSize={this.pageSize}
                   country="in"
                   category="health"
                 />
@@ -51,8 +66,9 @@ export class App extends Component {
               path="/sports"
               element={
                 <News
+                  setProgress={this.setProgress}
                   key="sports"
-                  pageSize={4}
+                  pageSize={this.pageSize}
                   country="in"
                   category="sports"
                 />
@@ -63,8 +79,9 @@ export class App extends Component {
               path="/technology"
               element={
                 <News
+                  setProgress={this.setProgress}
                   key="tech"
-                  pageSize={4}
+                  pageSize={this.pageSize}
                   country="in"
                   category="technology"
                 />
@@ -75,8 +92,9 @@ export class App extends Component {
               path="/business"
               element={
                 <News
+                  setProgress={this.setProgress}
                   key="business"
-                  pageSize={4}
+                  pageSize={this.pageSize}
                   country="in"
                   category="business"
                 />
@@ -87,8 +105,9 @@ export class App extends Component {
               path="/entertainment"
               element={
                 <News
+                  setProgress={this.setProgress}
                   key="entertainment"
-                  pageSize={4}
+                  pageSize={this.pageSize}
                   country="in"
                   category="entertainment"
                 />
